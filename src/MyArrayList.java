0f98a45958914f5ebc8e995a26ff0c003277e002
sokol-forbes <61580663+sokol-forbes@users.noreply.github.com>
2025-01-13 19:41:14 +0300

    добавлена документация

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,78 +1,149 @@
-import java.lang.Exception;
 
+
+/**
+ * Собственная реализация ArrayList<T>
+ *
+ * @author Alexander Sokolovsky
+ */
 public class MyArrayList<T> {
+    /**
+     * Вместительность
+     * Инициализированная размернотью 10
+     */
     private int capatity = 10;
-    private int size=0;
+    /**
+     * Размер списка
+     */
+    private int size = 0;
+    /**
+     * Массив объектов типа Т
+     */
     private T[] obj;
 
+    /**
+     * <p>Конструктор без параметров для инициализации списка стандартной размерности
+     * </p>
+     */
     public MyArrayList() {
         obj = (T[]) new Object[capatity];
-        //size = 0;
+        obj[0] = null;
     }
-    public T get(int index){return (T) obj[index];}
-    public void set(int index,T object)throws Exception{
-        if (index > size||index<0)
+
+    /**
+     * <p>Метод для доступа к объекту типа Т по индексу</p>
+     *
+     * @param index индекс необходимого объекта
+     * @return Объект типа Т
+     * @throws OutIndexException В случае невалидного индекса
+     */
+    public T get(int index) throws OutIndexException {
+        if (index > size || index < 0)
             throw new OutIndexException();
-        else obj[index-1]=object;
+        return (T) obj[index];
     }
-    public int size(){return size;}
-    public void add(T object){
-        obj[size++]=object;
+
+    /**
+     * <p>Метод для установки элемента в необходимую позицию</p>
+     *
+     * @param index позиция объекта который хотим изменить
+     * @throws OutIndexException В случае невалидного индекса
+     */
+    public void set(int index, T object) throws OutIndexException {
+        if (index > size || index < 0)
+            throw new OutIndexException();
+        else obj[index] = object;
     }
-    public void add(int index, T object) throws Exception {
-        if (index > size||index<=0)
+
+    /**
+     * <p>Метод для получения текущего размера коллекции</p>
+     * @return Текущий размер коллекции
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * <p>Метод для добавления объекта в коллекцию</p>
+     *
+     * @param object  Объект который необходимо добавить
+     * @return Объект типа Т
+     * @throws OutIndexException В случае невалидного индекса
+     */
+    public void add(T object) {
+        obj[size] = object;
+        size += 1;
+    }
+    /**
+     * <p>Метод для вставки объекта типа Т на необходимую полизицю с перекопированием следующих элементов</p>
+     * @param index позиция для вставки
+     * @param object Объект для вставки
+     * @throws OutIndexException В случае невалидного индекса
+     */
+    public void add(int index, T object) throws OutIndexException {
+        if (index > size || index <= 0)
             throw new OutIndexException();
         else {
             if (size == capatity) {
                 enlargement();
             }
-            //for (int i = index; i < size; i++)
-                //this.obj[i + 1] = this.obj[i];
 
-                System.arraycopy(obj,index,obj,index+1,size-index);
+            System.arraycopy(obj, index, obj, index + 1, size - index);
             this.obj[index] = object;
             size++;
 
-           // size++;
         }
 
     }
-
-    public void remove(int index) throws Exception {
+    /**
+     * <p>Метод для удаления объекта по индексу</p>
+     * @param index индекс необходимого объекта
+     * @throws OutIndexException В случае невалидного индекса
+     */
+    public void remove(int index) throws OutIndexException {
 
         if (index < size && index >= 0) {
-//            for (int i = index; i < size - 1; i++)
-                System.arraycopy(obj,index+1,obj,index,size-index-1);
-//                this.obj[i] = this.obj[i + 1];
+            System.arraycopy(obj, index + 1, obj, index, size - index - 1);
             size--;
         } else {
             throw new OutIndexException();
         }
 
     }
-
+    /**
+     * <p>Метод для расширения вместимости коллекции</p>
+     */
     private void enlargement() {
         capatity = (size * 3 / 2 + 1);
-        T[] objInstance = (T[])new Object[capatity];
+        T[] objInstance = (T[]) new Object[capatity];
         if (size >= 0)
             System.arraycopy(obj, 0, objInstance, 0, size);
         this.obj = (T[]) objInstance;
 
     }
-
-    public Object[] subList(int startIndex, int lastIndex) throws Exception {
+    /**
+     * <p>Метод для получения части коллекции</p>
+     * @param startIndex индекс начала части коллекции
+     * @param lastIndex индекс конца части коллекции
+     * @throws OutIndexException В случае невалидного индекса
+     * @return Возвращает подколлекцию
+     */
+    public Object[] subList(int startIndex, int lastIndex) throws OutIndexException {
         if (startIndex >= 0 && startIndex < lastIndex && lastIndex < size) {
             Object[] list = new Object[lastIndex - startIndex];
             for (int i = startIndex, j = 0; i < lastIndex; i++, j++)
                 list[j] = this.obj[i];
             return list;
-        }else throw new OutIndexException();
+        } else throw new OutIndexException();
     }
-    public String toString(){
-        String str="[";
-        for(int i=0;i<size;i++)
-            str+=(obj[i].toString()+" ");
-        str+="]";
+    /**
+     * <p>Метод для сериализации коллекции в строку</p>
+     * @return Возвращает строку содержащую объекты коллекции
+     */
+    public String toString() {
+        String str = "[";
+        for (int i = 0; i < size; i++)
+            str += (obj[i].toString() + " ");
+        str += "]";
         return str;
     }
 
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,42 +1,98 @@
+/**
+ * Собственная реализация LinkedList<T>
+ *
+ * @author Alexander Sokolovsky
+ */
 public class MyLinkedList<T> {
-
-
+    /**
+     * Поля хранищие данные первого и последнего узла списка
+     * Инициализированная размернотью 10
+     */
     private MyNode<T> start, end;
+    /**
+     * Поле для хранения текущего размера списка
+     */
     private Integer size;
 
+    /**
+     * Конструктор без параметров для инициализации списка нулевого размера
+     */
     public MyLinkedList() {
         size = 0;
         start = null;
         end = null;
     }
 
+    /**
+     * Конструктор  для инициализации списка
+     *
+     * @param start первый элемент списка
+     * @param end   последний элемент списка
+     * @param size  размер списка
+     */
     public MyLinkedList(MyNode<T> start, MyNode<T> end, Integer size) {
         this.start = start;
         this.end = end;
         this.size = size;
     }
 
+    /**
+     * Конструктор для инициализации списка с одним узлом
+     *
+     * @param node узел списка
+     */
+    public MyLinkedList(MyNode<T> node) {
+        this.start = node;
+        this.end = node;
+        this.size = 1;
+    }
+
+    /**
+     * Метод для получения текущего размера списка
+     *
+     * @return size Размер списка{@link MyLinkedList size}
+     */
     public Integer size() {
         return size;
     }
-
-    public void add(Integer index, T element) {
-
-    }
-
-    public void add(T element) {
-        MyNode node = new MyNode(element, null, null);
+    /**
+     * Метод для вставки элемента в список по индексу
+     * @param element вставляемы элемент
+     * @param index место вставки
+     */
+    public void add(Integer index, T element) throws OutIndexException {
+        MyNode<T> node = new MyNode<>(element, null, null);
         if (start == null && end == null) {
             start = node;
             end = node;
         } else {
-            end.setNext(node);
-            node.setPrev(end);
-            end = node;
+            MyNode<T> current = getNode(index);
+            node.setPrev(current.getPrev());
+            node.setNext(current);
+            current.setPrev(node);
+            current.getPrev().setNext(node);
         }
         size += 1;
     }
-
+    /**
+     * Метод для вставки элемента в конец списка
+     * @param element вставляемы элемент
+     */
+    public void add(T element) {
+        MyNode<T> node = new MyNode<T>(element, null, null);
+        if (start == null && end == null) {
+            start = node;
+        } else {
+            end.setNext(node);
+            node.setPrev(end);
+        }
+        end = node;
+        size += 1;
+    }
+    /**
+     * Метод для удаления элемента в списке по индексу
+     * @param index место удаления
+     */
     public void remove(Integer index) throws OutIndexException {
         if (index >= size || index < 0) {
             throw new OutIndexException();
@@ -48,18 +104,26 @@ public class MyLinkedList<T> {
             current.getNext().setPrev(current.getPrev());
         }
         if (index == 0)
-            start = start.getNext();
+            start = (MyNode<T>) start.getNext();
         if (index == size - 1)
-            end = end.getPrev();
+            end = (MyNode<T>)end.getPrev();
         size -= 1;
 
 
     }
-
+    /**
+     * Метод для получения элемента в списке по индексу
+     * @param index место необходимого элемента
+     * @return значение хранящееся в узле по индексу
+     */
     public T getValue(Integer index) throws OutIndexException {
         return (T) getNode(index).getValue();
     }
-
+    /**
+     * Метод для получения элемента в списке по индексу
+     * @param index место необходимого элемента
+     * @return значение хранящееся в узле по индексу
+     */
     public MyNode getNode(Integer index) throws OutIndexException {
         if (index >= size || index < 0) {
             throw new OutIndexException();
@@ -70,12 +134,13 @@ public class MyLinkedList<T> {
         }
         return current;
     }
-
-    public void set(T element) {
-        MyNode<T> node = new MyNode<T>(element, null, end);
-        end.setNext(node);
-    }
-
+    /**
+     * <p>Метод для получения части коллекции</p>
+     * @param startIndex индекс начала части коллекции
+     * @param lastIndex индекс конца части коллекции
+     * @throws OutIndexException В случае невалидного индекса
+     * @return Возвращает {@link MyLinkedList} c {@link @param startIndex} до {@link @param lastIndex}
+     */
     public MyLinkedList<T> subList(Integer startIndex, Integer lastIndex) {
         MyLinkedList<T> list = new MyLinkedList<>();
         try {
@@ -85,20 +150,23 @@ public class MyLinkedList<T> {
         }
         return list;
     }
-
+    /**
+     * <p>Метод для сериализации коллекции в строку</p>
+     * @return Возвращает строку содержащую объекты коллекции
+     */
     public String toString() {
-        String str = "[";
+        StringBuilder str = new StringBuilder("[");
         for (int i = 0; i < size; i++) {
 
             try {
-                str += (getValue(i) + ", ");
+                str.append(getValue(i)).append(", ");
             } catch (OutIndexException e) {
                 System.out.println(e.getMessage());
             }
 
         }
-        str += "]";
-        return str;
+        str.append("]");
+        return str.toString();
     }
 
 }
